52076d65f3582163878bf109b5d434c8a405bc0c
Anjan Prasad <anjanprasad112@gmail.com>
2023-10-05 21:50:19 +0530

    deque doubly ended queue

---
+++ queues/progam2.cpp
@@ -0,0 +1,17 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+class queue{
+
+};
+
+int main(){
+    deque<int> d;
+    d.push_back(1);
+    d.push_back(2);
+    d.push_front(3);
+
+    cout<< d.front() <<endl;
+    cout<< d.back() <<endl;
+    
+}
